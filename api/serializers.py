62568e653d41b2328fa95cb1742a4a314477790e
Yujing9 <244242977@qq.com>
2023-02-16 00:12:43 +0000

    tutorial7

--- api/serializers.py
+++ api/serializers.py
@@ -10,3 +10,4 @@ class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause','votes_to_skip')
+
--- api/views.py
+++ api/views.py
@@ -10,9 +10,28 @@ from rest_framework.response import Response
 #      return HttpResponse("Hello, world. You're at the app index.")
 
 # API view for django.contrib.auth.models.Room
+
+
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class GetRoom(APIView):
+    # 我们得将数据转化为python能看得懂的形式
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateRoomView(APIView):
